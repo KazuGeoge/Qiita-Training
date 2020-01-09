042522959b0f075121d7cf419058ed38020bc02e
KazuGeoge <punch.rocky2@gmail.com>
2020-01-09 22:03:47 +0900

    プロジェクト作成

---
+++ Qiita-Training/Qiita-Training/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Qiita-Training
+//
+//  Created by 城島一輝 on 2020/01/09.
+//  Copyright © 2020 城島一輝. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
