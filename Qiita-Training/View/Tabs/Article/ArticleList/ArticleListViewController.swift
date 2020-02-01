4a9e60019bb6b3abeb33f03ad5b1d5e16ff344d8
KazuGeoge <punch.rocky2@gmail.com>
2020-02-01 22:11:22 +0900

    各ViewControllerに必要な要素を追加

--- Qiita-Training/View/Tabs/Article/ArticleList/ArticleListViewController.swift
+++ Qiita-Training/View/Tabs/Article/ArticleList/ArticleListViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ArticleListViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
