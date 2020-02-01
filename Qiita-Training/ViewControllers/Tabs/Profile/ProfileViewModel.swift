26079e7ff7c97f6164cb580f708e9f05d0463ef5
KazuGeoge <punch.rocky2@gmail.com>
2020-02-01 21:14:07 +0900

    タブのRootとなる3つのViewControllerを作成

---
+++ Qiita-Training/ViewControllers/Tabs/Profile/ProfileViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileViewModel.swift
+//  Qiita-Training
+//
+//  Created by 城島一輝 on 2020/02/01.
+//  Copyright © 2020 城島一輝. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewModel: NSObject {
+
+}
---
+++ Qiita-Training/ViewControllers/Tabs/Search/SearchViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  SearchViewModel.swift
+//  Qiita-Training
+//
+//  Created by 城島一輝 on 2020/02/01.
+//  Copyright © 2020 城島一輝. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewModel: NSObject {
+
+}
