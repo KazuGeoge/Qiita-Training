79e4233f5cad84d79d5a8a348873e5446fdd58bf
KazuGeoge <punch.rocky2@gmail.com>
2020-02-01 23:03:06 +0900

    FluxでのRoute遷移に必要なファイルを作成

---
+++ Qiita-Training/Flux/Dispatcher/RouteDispatcher.swift
@@ -0,0 +1,13 @@
+//
+//  RouteDispatcher.swift
+//  Qiita-Training
+//
+//  Created by 城島一輝 on 2020/02/01.
+//  Copyright © 2020 城島一輝. All rights reserved.
+//
+
+import UIKit
+
+class RouteDispatcher: NSObject {
+
+}
