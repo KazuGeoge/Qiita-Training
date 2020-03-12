0137de7acbbdc7b2f6000a8ac0507cf51e2d20a7
KazuGeoge <punch.rocky2@gmail.com>
2020-03-12 19:54:31 +0900

    NSUserDefaultsを使用

---
+++ Qiita-Training/Model/UserDefaults.swift
@@ -0,0 +1,15 @@
+//
+//  UserDefaults.swift
+//  Qiita-Training
+//
+//  Created by 城島一輝 on 2020/03/12.
+//  Copyright © 2020 城島一輝. All rights reserved.
+//
+
+import UIKit
+import SwiftyUserDefaults
+
+extension DefaultsKeys {
+    var isLoginUdser: DefaultsKey<Bool> { return .init("isLoginUdser", defaultValue: false) }
+    var token: DefaultsKey<String?> { return .init("token") }
+}
