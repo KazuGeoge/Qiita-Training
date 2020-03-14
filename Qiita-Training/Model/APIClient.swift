3b3483f38b8132aa1bb5e8d8173df211caf8f1df
KazuGeoge <punch.rocky2@gmail.com>
2020-03-15 07:57:03 +0900

    APIClientを作成

--- Qiita-Training/Model/APIClient.swift
+++ Qiita-Training/Model/APIClient.swift
@@ -7,7 +7,32 @@
 //
 
 import UIKit
+import RxSwift
+import RxMoya
+import Moya
 
 class APIClient: NSObject {
 
+    static let shared = APIClient()
+    private let disposeBag = DisposeBag()
+
+    func call<T: Codable>(qiitaAPI: QiitaAPI) -> Observable<T> {
+        return Observable.create { observer in
+            
+            let provider = MoyaProvider<QiitaAPI>()
+            provider.rx.request(qiitaAPI)
+                .filterSuccessfulStatusCodes()
+                .map(T.self)
+                .subscribe(onSuccess: { profile in
+                    observer.onNext(profile)
+                }) { (error) in
+                    // TODO: エラーイベントを流す
+                    print(error)
+            }
+            .disposed(by: self.disposeBag)
+
+
+            return Disposables.create()
+        }
+    }
 }
--- Qiita-Training/Model/QiitaAPITargetType.swift
+++ Qiita-Training/Model/QiitaAPITargetType.swift
@@ -6,8 +6,44 @@
 //  Copyright © 2020 城島一輝. All rights reserved.
 //
 
-import UIKit
+import Moya
+import Foundation
 
-class QiitaAPITargetType: NSObject {
+enum QiitaAPI {
+    case newArticle
+}
 
+extension QiitaAPI: TargetType {
+    
+    var baseURL: URL {
+        // TODO: OptionalBindingの方法が分からない。nilで帰ってきたらどうするか？
+        return URL(string:"https://qiita.com")!
+    }
+    
+    var path: String {
+        switch self {
+        case .newArticle:
+            return "/api/v2/items"
+        }
+    }
+
+    var method: Moya.Method {
+        switch self {
+        case .newArticle:
+            return .get
+        }
+    }
+
+    //テストの際テスト用のjsonを返すことができるメソッド
+    var sampleData: Data {
+        return Data()
+    }
+
+    var task: Task {
+        return .requestPlain
+    }
+
+    var headers: [String : String]? {
+        return nil
+    }
 }
