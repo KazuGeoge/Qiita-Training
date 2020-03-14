0a3cfc9054a547a8478138d57d122da44c991316
KazuGeoge <punch.rocky2@gmail.com>
2020-03-15 05:53:36 +0900

    ファイル作成

---
+++ Qiita-Training/Flux/Dispatcher/ArticleDispatcher.swift
@@ -0,0 +1,13 @@
+//
+//  ArticleDispatcher.swift
+//  Qiita-Training
+//
+//  Created by 城島一輝 on 2020/03/15.
+//  Copyright © 2020 城島一輝. All rights reserved.
+//
+
+import UIKit
+
+class ArticleDispatcher: NSObject {
+
+}
---
+++ Qiita-Training/Model/APIClient.swift
@@ -0,0 +1,13 @@
+//
+//  APIClient.swift
+//  Qiita-Training
+//
+//  Created by 城島一輝 on 2020/03/15.
+//  Copyright © 2020 城島一輝. All rights reserved.
+//
+
+import UIKit
+
+class APIClient: NSObject {
+
+}
