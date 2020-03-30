7cda3b38cca14439868767381cefd22b0c93140d
KazuGeoge <punch.rocky2@gmail.com>
2020-03-30 10:54:22 +0900

    APIientではProviderを生成するのみにする

--- Qiita-Training/Model/APIClient.swift
+++ Qiita-Training/Model/APIClient.swift
@@ -6,33 +6,10 @@
 //  Copyright © 2020 城島一輝. All rights reserved.
 //
 
-import UIKit
-import RxSwift
-import RxMoya
 import Moya
 
 class APIClient: NSObject {
-
+    
     static let shared = APIClient()
-    private let disposeBag = DisposeBag()
-
-    func call<T: Codable>(qiitaAPI: QiitaAPI) -> Observable<T> {
-        return Observable.create { observer in
-            
-            let provider = MoyaProvider<QiitaAPI>()
-            provider.rx.request(qiitaAPI)
-                .filterSuccessfulStatusCodes()
-                .map(T.self)
-                .subscribe(onSuccess: { profile in
-                    observer.onNext(profile)
-                }) { (error) in
-                    // TODO: エラーイベントを流す
-                    print(error)
-            }
-            .disposed(by: self.disposeBag)
-
-
-            return Disposables.create()
-        }
-    }
+    let provider = MoyaProvider<QiitaAPI>()
 }
