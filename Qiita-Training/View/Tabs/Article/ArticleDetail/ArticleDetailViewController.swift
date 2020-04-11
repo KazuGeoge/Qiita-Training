32e7924b0598e7902942b21f9075bdb41f6a267d
KazuGeoge <punch.rocky2@gmail.com>
2020-04-11 14:12:07 +0900

    Labelを設定

--- Qiita-Training/View/Tabs/Article/ArticleDetail/ArticleDetailViewController.swift
+++ Qiita-Training/View/Tabs/Article/ArticleDetail/ArticleDetailViewController.swift
@@ -17,6 +17,7 @@ class ArticleDetailViewController: UIViewController {
     @IBOutlet private weak var userNameLabel: UILabel!
     @IBOutlet private weak var timeLabel: UILabel!
     @IBOutlet private weak var tagCollectionView: UICollectionView!
+    @IBOutlet private weak var tagLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
