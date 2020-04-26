0536e8afdcd1123f5172520697efcc6f84edf603
KazuGeoge <punch.rocky2@gmail.com>
2020-04-27 06:11:44 +0900

    dataSouceの初期化をViewControllerの初期化時に行う

--- Qiita-Training/View/Tabs/Article/ArticleDetail/ArticleDetailViewController.swift
+++ Qiita-Training/View/Tabs/Article/ArticleDetail/ArticleDetailViewController.swift
@@ -20,7 +20,7 @@ class ArticleDetailViewController: UIViewController, WKUIDelegate {
     @IBOutlet private weak var timeLabel: UILabel!
     @IBOutlet private weak var tagCollectionView: UICollectionView!
     @IBOutlet private weak var tagCollectionViewHeight: NSLayoutConstraint!
-    private var dataSouce: TagCollectionViewDataSouce?
+    private lazy var dataSouce = TagCollectionViewDataSouce(tagCollectionView: tagCollectionView)
     private let disposeBag = DisposeBag()
     private let viewModel = ArticleDetailViewModel()
     var article: Article?
@@ -41,8 +41,7 @@ class ArticleDetailViewController: UIViewController, WKUIDelegate {
     }
     
     private func configureDataSouce() {
-        dataSouce = TagCollectionViewDataSouce(tagCollectionView: tagCollectionView)
-        dataSouce?.configure(tagArray: article?.tags.map { $0.name } ?? [])
+        dataSouce.configure(tagArray: article?.tags.map { $0.name } ?? [])
     }
     
     override func viewDidLayoutSubviews() {
