e1fca1fae80d3032c01354a5e74cd2d3f8777eee
KazuGeoge <punch.rocky2@gmail.com>
2020-05-14 17:59:27 +0900

    StoreからのObservableをViewControllerにバインドする形に

--- Qiita-Training/View/Tabs/Article/ArticleList/ArticleListViewController.swift
+++ Qiita-Training/View/Tabs/Article/ArticleList/ArticleListViewController.swift
@@ -15,15 +15,22 @@ class ArticleListViewController: UIViewController {
     @IBOutlet private weak var tableView: UITableView!
     private lazy var viewModel = ArticleListViewModel()
     private lazy var dataSouce = ArticleListTableViewDataSouce(viewModel: viewModel)
+    private let disposeBag = DisposeBag()
     var qiitaAPIType: QiitaAPI?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        observeViewModel()
         dataSouce.configure(tableView: tableView)
-        
-        viewModel.observeArticleStore(qiitaAPIType: qiitaAPIType) { [weak self] in
-            self?.tableView.reloadData()
-        }
+    }
+    
+    private func observeViewModel() {
+        viewModel.observeArticleStore(qiitaAPIType: qiitaAPIType).asObservable()
+            .observeOn(MainScheduler.instance)
+            .subscribe(onNext: { [weak self] _ in
+                self?.tableView.reloadData()
+            })
+            .disposed(by: disposeBag)
     }
 }
