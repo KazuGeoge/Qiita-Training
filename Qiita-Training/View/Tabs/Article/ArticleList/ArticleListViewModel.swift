49daa23156cab7021b3564f9a7883043fbc9cd7c
KazuGeoge <punch.rocky2@gmail.com>
2020-05-16 23:07:38 +0900

    Revert "StoreからのObservableをViewControllerにバインドする形に"
    This reverts commit e1fca1fae80d3032c01354a5e74cd2d3f8777eee.

--- Qiita-Training/View/Tabs/Article/ArticleList/ArticleListViewModel.swift
+++ Qiita-Training/View/Tabs/Article/ArticleList/ArticleListViewModel.swift
@@ -13,24 +13,19 @@ final class ArticleListViewModel: NSObject {
 
     private let disposeBag = DisposeBag()
     var articleList: [Article] = []
-    var reloadTableView: Observable<()>
-    private let articleStore: ArticleStore
-    private let routeAction: RouteAction
     
-    init(articleStore: ArticleStore = .shared, routeAction: RouteAction = .shared) {
-        self.articleStore = articleStore
-        self.routeAction = routeAction
-    }
-    
-    func observeArticleStore(qiitaAPIType: QiitaAPI?) {
-                articleStore.article.asObservable()
-                    .filter { $0.1 == qiitaAPIType }
-                    .do(onNext: { [weak self] in self?.articleList = $0.0 })
-                    .bind(to: reloadTableView)
-                    .disposed(by: disposeBag)
+    func observeArticleStore(qiitaAPIType: QiitaAPI?, completion :@escaping () -> ()) {
+        ArticleStore.shared.article.asObservable()
+            .observeOn(MainScheduler.instance)
+            .filter { $0.1 == qiitaAPIType }
+            .subscribe(onNext: { [weak self] article in
+                self?.articleList = article.0
+                completion()
+            })
+            .disposed(by: disposeBag)
     }
     
     func showArticleDetail(article: Article) {
-        routeAction.show(routeType: .articleDetail(article))
+        RouteAction.shared.show(routeType: .articleDetail(article))
     }
 }
