887daa5666d760d9efccbd1a8305d9bf26522a5d
KazuGeoge <punch.rocky2@gmail.com>
2020-06-23 23:15:21 +0900

    profileのnavigationBarをExtensionで実装
    Extensionの実装方法はその他の箇所によって分岐方法を決めるため、後ほど実装する

--- Qiita-Training/Extension/NavigationItem.swift
+++ Qiita-Training/Extension/NavigationItem.swift
@@ -7,7 +7,33 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
-class NavigationItem: NSObject {
+enum NavigationItemType {
+    case rightTeam
+    case profile
+    case rightBack
+    case leftBack
+}
 
+extension UINavigationItem {
+
+    func configure(navigationItemType: NavigationItemType, disposeBag: DisposeBag) {
+        // TODO: その他の分岐方法は後ほど実装する。
+        let titleLabel = UILabel()
+        titleLabel.textColor = .white
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 20)
+        titleLabel.text = "プロフィール"
+        titleView = titleLabel
+        
+        let shareButtonItem = UIBarButtonItem(title: "⚙", style: .plain, target: self, action: nil)
+        rightBarButtonItems = [shareButtonItem]
+        
+        shareButtonItem.rx.tap
+            .subscribe(onNext: { _ in
+                print("設定のタップ")
+            })
+            .disposed(by: disposeBag)
+    }
 }
