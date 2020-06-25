969d682da55269c04d19c51861aeb38e5dd894e8
KazuGeoge <punch.rocky2@gmail.com>
2020-06-25 16:29:08 +0900

    ファイルの追加とStoryBoard名を修正

---
+++ Qiita-Training/View/Tabs/Profile/Setting/SettingTableViewDataSouce.swift
@@ -0,0 +1,13 @@
+//
+//  SettingTableViewDataSouce.swift
+//  Qiita-Training
+//
+//  Created by 城島一輝 on 2020/06/25.
+//  Copyright © 2020 城島一輝. All rights reserved.
+//
+
+import UIKit
+
+class SettingTableViewDataSouce: NSObject {
+
+}
