05f8de3318bddb7602495d7ec37556164dc54e21
KazuGeoge <punch.rocky2@gmail.com>
2020-07-01 17:27:47 +0900

    セルのStoryBoard設定とIBOutletでクラスのインスタンス変数に

--- Qiita-Training/Cell/Article/ArticleTableViewCell.swift
+++ Qiita-Training/Cell/Article/ArticleTableViewCell.swift
@@ -10,15 +10,12 @@ import UIKit
 
 class ArticleTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
+    @IBOutlet private weak var iconImageView: UIImageView!
+    @IBOutlet private weak var userNameLabel: UILabel!
+    @IBOutlet private weak var articleTitleLabel: UILabel!
+    @IBOutlet private weak var favCountView: UIView!
+    @IBOutlet private weak var favCountLabel: UILabel!
+    @IBOutlet private weak var tagsView: UIView!
+    @IBOutlet private weak var tagsLabel: UILabel!
     
 }
