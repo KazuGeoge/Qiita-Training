24b6ef5ed95d7c490352fbf8930409d657d9b82d
KazuGeoge <punch.rocky2@gmail.com>
2020-07-01 16:32:32 +0900

    記事用のセルファイルを作成

---
+++ Qiita-Training/Cell/Article/ArticleTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ArticleTableViewCell.swift
+//  Qiita-Training
+//
+//  Created by 城島一輝 on 2020/07/01.
+//  Copyright © 2020 城島一輝. All rights reserved.
+//
+
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
