fe22fcaf0ed9d81259d368afdb687bd121da5977
KazuGeoge <punch.rocky2@gmail.com>
2020-07-08 22:39:15 +0900

    IBOでインスタンス変数化

--- Qiita-Training/Cell/User/UserTableViewCell.swift
+++ Qiita-Training/Cell/User/UserTableViewCell.swift
@@ -10,15 +10,7 @@ import UIKit
 
 class UserTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-    
+    @IBOutlet private weak var iconImageView: UIImageView!
+    @IBOutlet private weak var userNameLabel: UILabel!
+    @IBOutlet private weak var descriptionLabel: UILabel!
 }
