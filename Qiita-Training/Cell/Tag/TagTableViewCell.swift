995a6e1d6b0b7c80a65b09914db4d611cc524fe5
KazuGeoge <punch.rocky2@gmail.com>
2020-07-09 15:22:19 +0900

    ファイル追加とStoryBoardの作成

---
+++ Qiita-Training/Cell/Tag/TagTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TagTableViewCell.swift
+//  Qiita-Training
+//
+//  Created by 城島一輝 on 2020/07/09.
+//  Copyright © 2020 城島一輝. All rights reserved.
+//
+
+import UIKit
+
+class TagTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
