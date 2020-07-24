af920fb90518826da10918bb87a182272d0db822
KazuGeoge <punch.rocky2@gmail.com>
2020-07-24 18:39:24 +0900

    ボタンのカスタムクラスを追加

---
+++ Qiita-Training/Button/FollowButton.swift
@@ -0,0 +1,18 @@
+//
+//  FollowButton.swift
+//  Qiita-Training
+//
+//  Created by 城島一輝 on 2020/07/24.
+//  Copyright © 2020 城島一輝. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class FollowButton: UIButton {
+
+    override func draw(_ rect: CGRect) {
+        
+    }
+}
